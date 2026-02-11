74f813db35d6b7cb13217d64245529e7a6481c29
Oksana <oksana20994@gmail.com>
2026-02-11 19:27:35 +0300

    Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -1,11 +1,12 @@
 package guru.qa;
 
 public class Main {
+   
     private static String msg = "Hello, qa guru!";
-    public static void main(String[] args) {
+    
+    public static void main(String... args) {
         System.out.println(getMessage());
     }
-
 
     private  static String getMessage () {
         return msg;
